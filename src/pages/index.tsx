b2cdba4a40936cf80d144f678ab361c16f7ea48d
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2021-04-21 16:45:51 -0400

    Add type for episodes and convert duration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
+import { GetStaticProps } from 'next';
+import {format, parseISO} from 'date-fns';
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
+}
 
-export default function Home(props) {
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -10,13 +29,32 @@ export default function Home(props) {
 }
 
 //SSG - Static Site Generation
-export async function getStaticProps(){
-  const res = await fetch('http://localhost:3333/episodes')
-  const data = await res.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const {data} = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'MMM d yyyy'),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  })
 
   return {
     props: {
-      episodes: data,
+      episodes: episodes,
     },
     revalidate: 60 * 60 * 8,
   }
